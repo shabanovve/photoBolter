09ee7cd7a45e838699d40bd003f252060e378c78
Владимир Шабанов <v.shabanov@swemel.ru>
2018-11-15 20:20:23 +0400

    Create stage initialization

--- photoBolter/src/main/java/ru/photoBolter/App.java
+++ photoBolter/src/main/java/ru/photoBolter/App.java
@@ -1,13 +1,29 @@
 package ru.photoBolter;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
     }
+
+    public void start(Stage stage) {
+        //create tree pane
+        VBox treeBox = new VBox();
+        treeBox.setPadding(new Insets(10, 10, 10, 10));
+        treeBox.setSpacing(10);
+
+        //setup and show the window
+        stage.setTitle("JavaFX File Browse Demo");
+        StackPane root = new StackPane();
+        root.getChildren().addAll(treeBox);
+        stage.setScene(new Scene(root, 400, 300));
+        stage.show();
+    }
+
 }
