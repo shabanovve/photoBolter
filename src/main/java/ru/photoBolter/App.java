1638f9f308b2d79eef28f19681d019edaa9fe4fb
Vladimir Shabanov <shabanovve@mail.ru>
2018-11-17 20:42:49 +0400

    Add tree view

--- src/main/java/ru/photoBolter/App.java
+++ src/main/java/ru/photoBolter/App.java
@@ -1,29 +1,43 @@
 package ru.photoBolter;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
+    private final Node folderIcon = new ImageView(
+            new Image(getClass().getResourceAsStream("../../folder.png"))
+    );
+
+    private final Node fileIcon = new ImageView(
+            new Image(getClass().getResourceAsStream("../../file.png"))
+    );
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void start(Stage stage) {
-        //create tree pane
-        VBox treeBox = new VBox();
-        treeBox.setPadding(new Insets(10, 10, 10, 10));
-        treeBox.setSpacing(10);
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Tree View Sample");
 
-        //setup and show the window
-        stage.setTitle("JavaFX File Browse Demo");
+        TreeItem<String> rootItem = new TreeItem<>("Inbox", folderIcon);
+        rootItem.setExpanded(true);
+        for (int i = 1; i < 6; i++) {
+            TreeItem<String> item = new TreeItem<>("Message" + i, fileIcon);
+            rootItem.getChildren().add(item);
+        }
+        TreeView<String> tree = new TreeView<>(rootItem);
         StackPane root = new StackPane();
-        root.getChildren().addAll(treeBox);
-        stage.setScene(new Scene(root, 400, 300));
-        stage.show();
+        root.getChildren().add(tree);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 
 }
