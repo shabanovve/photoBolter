a0aedf682059e2334e667e04c41501285c97df12
Vladimir Shabanov <shabanovve@mail.ru>
2018-11-17 21:36:42 +0400

    Add DirectoryChooser

--- src/main/java/ru/photoBolter/App.java
+++ src/main/java/ru/photoBolter/App.java
@@ -1,14 +1,20 @@
 package ru.photoBolter;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class App extends Application {
 
@@ -24,8 +30,23 @@ public class App extends Application {
         launch(args);
     }
 
-    public void start(Stage primaryStage) {
+    public void start(final Stage primaryStage) {
         primaryStage.setTitle("Tree View Sample");
+
+        final DirectoryChooser directoryChooser = new DirectoryChooser();
+
+        final Button openButton = new Button("Open a Picture...");
+
+
+        openButton.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(final ActionEvent e) {
+                        directoryChooser.showDialog(primaryStage);
+                    }
+                });
+
+
 
         TreeItem<String> rootItem = new TreeItem<>("Inbox", folderIcon);
         rootItem.setExpanded(true);
@@ -36,6 +57,7 @@ public class App extends Application {
         TreeView<String> tree = new TreeView<>(rootItem);
         StackPane root = new StackPane();
         root.getChildren().add(tree);
+        root.getChildren().add(openButton);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
